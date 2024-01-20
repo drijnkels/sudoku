4fca5069770383a5c68e636c312556c15e2a3a32
Daan Rijnkels <daanrijnkels@gmail.com>
2024-01-20 11:04:55 -0700

    Create test for the Button component

---
+++ __tests__/components/Button.test.tsx
@@ -0,0 +1,28 @@
+import { describe, it, expect, vi } from 'vitest'
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+
+import Button from "@/components/Controls/Button";
+
+describe('Button', () => {
+
+	it('should shown childern', () => {
+		render (
+      <Button>8</Button>
+    );
+    expect(screen.getByText(8)).toBeDefined();
+	})
+
+  it('should show given digit', async () => {
+		const mockOnClick = vi.fn();
+    render (
+      <Button onClick={mockOnClick}>Test</Button>
+    );
+
+		const button = screen.getByText('Test');
+    await userEvent.click(button);
+
+    expect(mockOnClick).toHaveBeenCalled();
+  })
+
+})
