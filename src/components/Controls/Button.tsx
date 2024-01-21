6da182c636ac51e8f62a7377c479b2c5febf9a32
drijnkels <daanrijnkels@gmail.com>
2024-01-20 21:39:34 -0700

    Add aria properties to Buttons and change div to button for improve accessibility and pass the Controls tests

--- src/components/Controls/Button.tsx
+++ src/components/Controls/Button.tsx
@@ -8,7 +8,7 @@ type Button = {
 export default function Button ({children, type = "button", ariaToggle = false, ariaLabel, onClick}: Button){
   return (
     <button
-      type="button"
+      type={type}
       onClick={onClick}
       className='flex justify-center items-center h-[75px] w-[75px] bg-sky-100 border border-slate-300 rounded-lg hover:bg-sky-400 cursor-pointer transition-colors'
       aria-pressed={ariaToggle ? false : undefined}
