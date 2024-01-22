32098fd53d1e4a083f48b9e5e5d6449381116045
Daan Rijnkels <daanrijnkels@gmail.com>
2024-01-22 14:13:20 -0700

    Fix error in validateBoard test and also test a board with a mistake

--- __tests__/scripts/validateBoard.test.ts
+++ __tests__/scripts/validateBoard.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-import { mockEasyBoard } from '../mocks/mockSudokus';
+import { mockEasyBoard, mockErrorBoard, invalidBoard } from '../mocks/mockSudokus';
 import { stringToBoard, validateBoard } from "@/scripts/utils";
 
 describe('validateBoard', () => {
@@ -10,8 +10,14 @@ describe('validateBoard', () => {
     expect(validateBoard(boardData)).toBe(true);
   });
 
+  it('should return false for a board with a mistake', () => {
+    const boardData = stringToBoard(mockErrorBoard);
+    /* @ts-ignore */
+    expect(validateBoard(boardData)).toBe(false);
+  });
+
   it('should return false for an invalid board', () => {
-    const boardData = stringToBoard(mockEasyBoard);
+    const boardData = stringToBoard(invalidBoard);
     /* @ts-ignore */
     expect(validateBoard(boardData)).toBe(false);
   });
