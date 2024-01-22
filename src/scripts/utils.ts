5b41fcc833510cda10b4c6cc48c75e4bd7defb52
Daan Rijnkels <daanrijnkels@gmail.com>
2024-01-22 14:15:08 -0700

    Add util method to find mistakes on the board

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -9,5 +9,47 @@ export const stringToBoard = (boardString: string) => {
 }
 
 export const validateBoard = (boardData: number[][]) => {
+    // Test board validity
+    if (!boardData) {
+        console.error('Invalid board');
+        return false;
+    }
+
+    const validateGroup = (group: number[]) => {
+        group = group.filter((n) => n != 0);
+        return group.length === new Set(group).size;
+    }
+
+    // Check rows
+    for (let row of boardData) {
+        if(!validateGroup(row)){
+            return false;
+        }
+    }
+
+    // Check columns
+    for (let col = 0; col < 9; col++) {
+        const column = boardData.map((row) => row[col]);
+        if(!validateGroup(column)){
+            return false;
+        }
+    }
+
+    // Check squares
+    for (let r = 0; r < 9; r += 3) {
+        for (let c = 0; c < 9; c += 3) {
+            const square = [];
+            for (let y = r; y < r + 3; y++) {
+                for (let x = c; x < c + 3; x++) {
+                    square.push(boardData[y][x]);
+                }
+            }
+
+            if(!validateGroup(square)){
+                return false;
+            }
+        }
+    }
+
     return true;
 }
