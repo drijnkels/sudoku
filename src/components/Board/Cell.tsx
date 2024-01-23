f4cd70b1a263550dd955399e08f3f2a7d9706aea
drijnkels <daanrijnkels@gmail.com>
2024-01-22 23:01:13 -0700

    Handle obvious errors and game completion

--- src/components/Board/Cell.tsx
+++ src/components/Board/Cell.tsx
@@ -1,25 +1,39 @@
 import Notes from "@/components/Board/Notes";
+import {GridLoc} from "@/types/types";
 
 export type CellType = {
   cellIndex: number,
   digit:number,
   highlight:string,
   selectCell: () => void
+  errorCell: GridLoc | undefined,
+  gameComplete: boolean,
   notes: number[]
 }
 
-export default function Cell({cellIndex, digit, highlight, selectCell, notes} : CellType) {
-
+export default function Cell({cellIndex, digit, highlight, selectCell, errorCell, gameComplete, notes} : CellType) {
+  const getBorderClass = () => {
+    return cellIndex % 3 === 2 && cellIndex !== 8 ? 'border-r border-slate-800' : 'border-r border-slate-300';
+  }
+  const getBackgroundClass = () => {
+    if (gameComplete) {
+      return 'bg-emerald-200';
+    } else if (errorCell) {
+      return 'bg-red-500/30 cursor-pointer';
+    } else if (highlight) {
+      return highlight + ' cursor-pointer';
+    }
+    return 'hover:bg-slate-400/30 cursor-pointer'
+  }
   return (
       <div
         onClick={selectCell}
-        className={`relative flex items-center justify-center w-[65px] h-[65px] text-4xl cursor-pointer transition-colors hover:bg-slate-400/30
-          ${cellIndex % 3 === 2 && cellIndex != 8 ? 'border-r border-slate-800' : 'border-r border-slate-300'}
-          ${digit !== 0 ? 'text-blue-700 font-bold' : ''}
-          ${highlight}
+        className={`relative flex items-center justify-center w-[65px] h-[65px] text-4xl transition-colors text-blue-700 font-bold
+          ${getBorderClass}
+          ${getBackgroundClass()}
       `}>
           {digit !== 0 ? digit : ''}
-        <Notes notes={notes} />
+        <Notes notes={digit === 0 ? notes : []} />
       </div>
   )
 }
