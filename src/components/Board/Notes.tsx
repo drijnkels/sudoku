ad4e145e9a995b223e7856ef0fc07f4a45cc12ee
drijnkels <daanrijnkels@gmail.com>
2024-01-23 23:18:44 -0700

    Increase note font size and make font gray

--- src/components/Board/Notes.tsx
+++ src/components/Board/Notes.tsx
@@ -5,7 +5,7 @@ export default function Notes ({notes}: {notes:number[]}) {
     [7,8,9]
   ]
   return (
-    <div className='absolute inset-0 text-[10px] leading-5 text-center grid grid-rows-3'>
+    <div className='absolute inset-0 text-[14px] leading-5 text-slate-500 text-center grid grid-rows-3'>
       {
         noteDigits.map((row, rowIndex) => (
           <div key={rowIndex} className='flex'>
