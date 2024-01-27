81cf742942c52530bd43ab728f85d9fdedd231a1
drijnkels <daanrijnkels@gmail.com>
2024-01-26 21:28:22 -0700

    Add a hard board and fixes a few small issues with the boards file

--- src/components/boards/boards.ts
+++ src/components/boards/boards.ts
@@ -1,23 +1,27 @@
 import {Puzzle} from "@/types/types";
 
 export const easyBoards: Puzzle[] = [
-  {puzzle_id: 'easy_1', url: '/easy/easy_1', name: 'Puzzle 1', board: '802605371 415803290 000901004 061080030 209034000 000002007 024000708 600010000 000008620', completion: '0%'},
-  {puzzle_id: 'easy_2', url: '/easy/easy_2', name: 'Puzzle 2', board: '380000004 704506000 000003060 000208015 010630040 007000008 176804309 005000206 820965401', completion: '0%'},
-  {puzzle_id: 'easy_3', url: '/easy/easy_3', name: 'Puzzle 3', board: '290004360 640092008 030051902 000700004 103405809 400008000 906580020 700240096 024900087', completion: '0%'},
-  {puzzle_id: 'easy_4', url: '/easy/easy_4', name: 'Puzzle 4', board: '096040030 057820000 100900500 009010008 500000002 400090600 004003001 000079260 020050980', completion: '0%'},
-  {puzzle_id: 'easy_5', url: '/easy/easy_5', name: 'Puzzle 5', board: '007000908 030170004 000006000 698740300 003010400 001039762 000400000 900051040 405000100', completion: '0%'},
-  {puzzle_id: 'easy_6', url: '/easy/easy_6', name: 'Puzzle 6', board: '000009006 000003851 062015000 007000060 021976380 030000100 000450970 258600000 400300000', completion: '0%'},
-  {puzzle_id: 'easy_7', url: '/easy/easy_7', name: 'Puzzle 7', board: '290074000 010000400 670905000 080206000 060847020 000501080 000708092 006000010 000410058', completion: '0%'},
-  {puzzle_id: 'easy_demo', url: '/easy/easy_demo', name: 'Completion Demo', board: '892645371 415873296 376921854 761589432 289734165 543162987 924356718 638217549 157498620', completion: '0%'},
+  {puzzle_id: 'easy_1', url: '/easy/easy_1', name: 'Puzzle 1', board: '802605371 415803290 000901004 061080030 209034000 000002007 024000708 600010000 000008620'},
+  // {puzzle_id: 'easy_2', url: '/easy/easy_2', name: 'Puzzle 2', board: '380000004 704506000 000003060 000208015 010630040 007000008 176804309 005000206 820965401'},
+  {puzzle_id: 'easy_3', url: '/easy/easy_3', name: 'Puzzle 3', board: '290004360 640092008 030051902 000700004 103405809 400008000 906580020 700240096 024900087'},
+  {puzzle_id: 'easy_4', url: '/easy/easy_4', name: 'Puzzle 4', board: '096040030 057820000 100900500 009010008 500000002 400090600 004003001 000079260 020050980'},
+  {puzzle_id: 'easy_5', url: '/easy/easy_5', name: 'Puzzle 5', board: '007000908 030170004 000006000 698740300 003010400 001039762 000400000 900051040 405000100'},
+  {puzzle_id: 'easy_6', url: '/easy/easy_6', name: 'Puzzle 6', board: '000009006 000003851 062015000 007000060 021976380 030000100 000450970 258600000 400300000'},
+  {puzzle_id: 'easy_7', url: '/easy/easy_7', name: 'Puzzle 7', board: '290074000 010000400 670905000 080206000 060847020 000501080 000708092 006000010 000410058'},
+  {puzzle_id: 'easy_demo', url: '/easy/easy_demo', name: 'Completion Demo', board: '892645371 415873296 376921854 761589432 289734165 543162987 924356718 638217549 157498620'},
 ];
 
-export const mediumBoard = [
+export const mediumBoards: Puzzle[] = [
   {puzzle_id: 'medium_1', url: '/medium/medium_1', name: 'Medium 1', board: '690003008 005002490 000408000 960000000 030604020 000000067 000205000 046900800 200700039'},
   {puzzle_id: 'medium_2', url: '/medium/medium_2', name: 'Medium 2', board: '100060057 805000400 000005000 007600009 530908074 900004200 000100000 004000601 290050003'},
 ];
-export const hardBoards = [];
-export const evilBoards = [
-  {puzzle_id: 'evil_1', url:'/evil/evil_1', name: 'Puzzle 1', board: '090000005 100900030 002800970 804030000 910000053 000060807 041008300 060004001 300000020', completion: '0%'},
-  {puzzle_id: 'evil_2', url:'/evil/evil_2', name: 'Puzzle 2', board: '091040000 068700350 700020000 000800037 004000100 680001000 000070003 012003970 000080420', completion: '0%'},
+
+export const hardBoards = [
+  {puzzle_id: 'hard_1', url: '/hard/hard_1', name: 'Hard 1', board: '040600082 006009010 507000000 000040079 008000100 170050000 000000301 010200800 320005090'},
+];
+
+export const evilBoards: Puzzle[] = [
+  {puzzle_id: 'evil_1', url:'/evil/evil_1', name: 'Puzzle 1', board: '090000005 100900030 002800970 804030000 910000053 000060807 041008300 060004001 300000020'},
+  {puzzle_id: 'evil_2', url:'/evil/evil_2', name: 'Puzzle 2', board: '091040000 068700350 700020000 000800037 004000100 680001000 000070003 012003970 000080420'},
 ]
 
