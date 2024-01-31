944c0ae37b5927527d94c8e66c85a55eb4841914
drijnkels <daanrijnkels@gmail.com>
2024-01-31 16:49:06 -0700

    Implement test for initialBoards to prevent data entry issues

--- __tests__/scripts/testBoards.test.ts
+++ __tests__/scripts/testBoards.test.ts
@@ -1,36 +1,36 @@
 import { describe, it, expect } from 'vitest';
 
-import { easySolutions, mediumSolutions, hardSolutions, evilSolutions } from '@/components/boards/solutions';
+import { easyBoards, mediumBoards, hardBoards, evilBoards } from '@/components/boards/boards';
 import { stringToBoard, validateBoard } from "@/scripts/utils";
 
 describe('validateSolutions', () => {
   it('all easy solutions should be valid board', () => {
-    for(let solution of easySolutions) {
-      const boardData = stringToBoard(solution.board);
+    for(let board of easyBoards) {
+      const boardData = stringToBoard(board.board);
       /* @ts-ignore */
       expect(validateBoard(boardData)).toBe(true);
     }
   });
 
   it('all medium solutions should be valid board', () => {
-    for(let solution of mediumSolutions) {
-      const boardData = stringToBoard(solution.board);
+    for(let board of mediumBoards) {
+      const boardData = stringToBoard(board.board);
       /* @ts-ignore */
       expect(validateBoard(boardData)).toBe(true);
     }
   });
 
   it('all hard solutions should be valid board', () => {
-    for(let solution of hardSolutions) {
-      const boardData = stringToBoard(solution.board);
+    for(let board of hardBoards) {
+      const boardData = stringToBoard(board.board);
       /* @ts-ignore */
       expect(validateBoard(boardData)).toBe(true);
     }
   });
 
   it('all evil solutions should be valid board', () => {
-    for(let solution of evilSolutions) {
-      const boardData = stringToBoard(solution.board);
+    for(let board of easyBoards) {
+      const boardData = stringToBoard(board.board);
       /* @ts-ignore */
       expect(validateBoard(boardData)).toBe(true);
     }
