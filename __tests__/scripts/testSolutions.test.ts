2769d2cfa024277a0d0ac5de8db971ee3530c710
drijnkels <daanrijnkels@gmail.com>
2024-01-30 18:09:31 -0700

    Moar boards and add testing for hardboards

--- __tests__/scripts/testSolutions.test.ts
+++ __tests__/scripts/testSolutions.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-import { easySolutions, mediumSolutions, evilSolutions } from '@/components/boards/solutions';
+import { easySolutions, mediumSolutions, hardSolutions, evilSolutions } from '@/components/boards/solutions';
 import { stringToBoard, validateBoard } from "@/scripts/utils";
 
 describe('validateSolutions', () => {
@@ -22,8 +22,13 @@ describe('validateSolutions', () => {
     }
   });
 
-  it.todo('all hard solutions should be valid board', () => {
-
+  it('all hard solutions should be valid board', () => {
+    for(let solution of hardSolutions) {
+      const boardData = stringToBoard(solution.board);
+      console.log(solution.board);
+      /* @ts-ignore */
+      expect(validateBoard(boardData)).toBe(true);
+    }
   });
 
   it('all evil solutions should be valid board', () => {
